d0619ffe6122cb92dd821db22a79e8e64f316327
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-09-07 19:53:09 +0300

    Avastasin, et tegin osad muudatused valesse faili. Tehtud: Lisatud registreerumise vorm, php ja html, lLisatud div tagid

--- userform.php
+++ userform.php
@@ -1,8 +1,6 @@
 
-<?php
+<?php 
 
-	$email_error = "";
-	$password_error = "";
 	/* user_form.php
 	Jutumarkide vahele input elemendi NAME
 	
@@ -10,26 +8,50 @@
 	echo $_POST["password"];*/
 
 	//Kontrolli ss kui kasutaja vajutab submit
+
+	//Logimise errorid
+	$email_error = "";
+	$password_error = "";
+	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		
-		//Kontrolli kasutaja e-posti ja parooli, et see poleks tühi
+	//Kontrolli kasutaja e-posti ja parooli, et see poleks tühi
 		if (empty($_POST["email"])) {
-			$email_error = "Teie e-posti lünk on tühi";
+			$email_error = "Palun sisesta e-posti aadress!";
 		
 		}	
 		if (empty($_POST["password"])) {
-			$password_error = "Teie parooli lünk on tühi";
+			$password_error = "Palun sisesta parool!";
 		
-		}	else {
-			if (strlen($_POST["password"]) < 8) {
-				echo "Peab olema vähemalt 8 sümbolit pikk";
-				
-			}
-			
 		}
-		
 	}
 	
+	//Registreerumise errorid
+	
+	
+	$reg_email_error = "";
+	$reg_password_error = "";
+	$reg_password_repeat_error = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+			//Kontrolli kasutaja e-posti ja parooli, et see poleks tühi
+			if (empty($_POST["reg_email"])) {
+				$reg_email_error = "Palun sisesta e-posti aadress!";
+				
+			}	
+			if (empty($_POST["reg_password"])) {
+				$reg_password_error = "Palun sisesta parool!";
+				
+			}
+			if (empty($_POST["reg_password_repeat"])) {
+			$reg_password_repeat_error = "Teie parool ei kattunud eelneva parooliga!";
+				
+			}
+		}
+	
+	
+	
+
 
 
 
@@ -41,15 +63,29 @@
 		<title>UCP</title>
 	</head>
 	<body>
-		<form action="userform.php" method="post">
-		<h2>Logi sisse</h2>
-		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
-		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
+	
+		<div id="login">
+			<form action="userform.php" method="post">
+				<h2>Logi sisse</h2>
+				<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+				<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
+				
+				<input type="submit" value="Logi sisse">
+			</form>
+		</div>
 		
-		<input type="submit" value="Logi sisse">
-		</form>
 		
-		<h2>Registreeru</h2>
+	 	<div id="register">
+			<form action="userform.php" method="post">
+				<h2>Registreeru</h2>
+				<input name="reg_email" type="email" placeholder="E-post"> <?php echo $reg_email_error; ?><br><br>
+				<input name="reg_password" type="password" placeholder="Parool"> <?php echo $reg_password_error; ?><br><br>
+				<input name="reg_password_repeat" type="password" placeholder="Korda parooli"> <?php echo $reg_password_repeat_error; ?><br><br>
+				
+				<input type="submit" value="Registreeru">
+			</form>
+		</div>
+		
 		
 	</body>
 </html>
