a81f07b9ea89d597b4b69cddf9390b385afe2806
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-09-08 12:13:11 +0300

    new file

--- User_form.php
+++ User_form.php
@@ -5,22 +5,24 @@
 		
 		//echo $_POST["email"];
 		
-		//echo $_POST["Password"];
+		//echo $_POST["password"];
 		
 		$email_error = "";
+		$password_error = "";
+		//$password_len_error = "";
 		
 		//kontrolli ainult siis, kui kasutaja vajutab logi sisse nuppu
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
 			if(empty($_POST["email"]))	{
 				$email_error = "ei saa olla tühi";
 			}	
-			if(empty($_POST["Password"]))
-		$password_error = "Teie parooli lünk on tühi";
-	
+			if(empty($_POST["password"])) {
+				$password_error = "Teie parooli lünk on tühi";
+			}
 				//parool ei saa olla tühi, kontrollime pikkust
-				if(strlen($_POST["Password"]) < 8 ) {
-				
-					$password_error = "Peab olema vähemalt 8 sümbolit pikk";
+			if(strlen($_POST["password"]) < 8 ) {
+				$password_error = "Peab olema vähemalt 8 sümbolit pikk";
+			}
 		}
 ?>
 <html>
@@ -32,12 +34,17 @@
 		<h2>Login</h2>
 		<form action="User_form.php" method="post">
 		
-		<input name="email" type="Email" placeholder="E-post">* <?php echo $email_error; ?> <br> <br> 
-		<input name="Password" type="Password" placeholder="Password">	
+		<input name="email" type="email" placeholder="E-post">* <?php echo $email_error; ?> <br> <br> 
+		<input name="password" type="password" placeholder="Password">	
 		
-		<input type ="submit" value ="logi sisse">
+		<input type="submit" value="logi sisse">
 		</form>
 		<h2>Create user</h2>
+	<form action="User_form.php" method="post">
 		
+		<input name="isikukood" type="isikukood" placeholder="isikukood">* <?php echo $email_error; ?> <br> <br> 
+		<input name="isikukood" type="vanus" placeholder="vanus">	
+		
+		<input type="submit" value="logi sisse">		
 	</body>
 </html>
