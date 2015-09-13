2874e3664ad47bdc28cbacc97f40f848efd532b9
Brendon Brant <brenbra@greeny.cs.tlu.ee>
2015-09-13 22:20:16 +0300

    hopefully finished homework

--- user_form.php
+++ user_form.php
@@ -4,6 +4,14 @@ echo $_POST["email"];
 	$email_error = "";
 echo $_POST["password"];
 	$password_error ="";
+echo $_POST["username"];
+	$username_error ="";
+echo $_POST["newemail"];
+	$email_error2 ="";
+echo $_POST["new_password"];
+	$new_password_error ="";
+echo $_POST["confirm_password"];
+	$confirm_password_error ="";
 	
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		
@@ -12,11 +20,23 @@ echo $_POST["password"];
 			$email_error ="Unavailable";
 			if(empty($_POST["password"])) {	
 				$password_error = "Also unavailable";
+				if(empty($_POST["username"])) {
+						$username_error = "Please enter an username";
+						if(empty($_POST["newemail"])) {
+								$email_error2 = "Please add your E-mail";
+								if(empty($_POST["new_password"])) {
+										$new_password_error = "Please enter a password";
+										if(empty($_POST["confirm_password"])) {
+											$confirm_password_error = "Does not match";
+												
+											}
+									}
+							}
+					}
 			}
-			
 		}
-	
 	}
+	
 ?>
 <html>
 	<head>
@@ -31,6 +51,12 @@ echo $_POST["password"];
 	<input type="submit" value="Log in"> 
 	</form>
 	<h2>Create user</h2>
-
+	<form action="user_form.php" method="post">
+	<input name="username" type="text" placeholder="New username"> <?php echo $username_error; ?> <br>
+	<input name="newemail" type="email" placeholder="Email"> <?php echo $email_error2; ?> <br>
+	<input name="new_password" type="password" placeholder="New password"> <?php echo $new_password_error; ?> <br>
+	<input name="confirm_password" type="password" placeholder="Confirm password"> <?php echo $confirm_password_error; ?> <br>
+	<input type="submit" value="Create user">
+	</form>
 	</body>
-</html> 
+</html>
