64d3c29097858c8f489ce36be60ee2845adbee9c
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-09-13 23:10:21 +0300

    kelly muutis kodus natuke

--- user_form.php
+++ user_form.php
@@ -5,8 +5,10 @@
 	//jutumärkide vahele input elemendi NAME
 	//echo $_POST["email"];
 	//echo $_POST["password"];
-	
+		
 	$email_error = "";
+	$password_error = "";
+	$eesnimi_error = "";
 	
 	
 	//Kontrolli ainult siis kui kasutaja vajutab logi sisse nuppu
@@ -19,7 +21,7 @@
 		}	
 		
 		//Kontrolli parooli
-		if(empty($_POST[password])) {
+		if(empty($_POST["password"])) {
 			$password_error = "sisesta parool";
 		} else {
 			//Parool ei ole tühi, kontrollime pikkust
@@ -27,6 +29,11 @@
 				
 				$password_error = "Peab olema vähemalt 8 sümbolit pikk";
 			}
+		}
+		
+		//ega nimi ei ole tühi
+		if (empty($_POST["eesnimi"])) {
+			$eesnimi_error = "Palun sisesta eesnimi";
 		}
 	}
 ?>
@@ -37,14 +44,24 @@
 	</head>
 	<body>
 	
-		<h2>Login</h2>
+		<h2>Logi sisse</h2>
 		<form action="user_form.php" method="post">
-			<input name="email" type= "email"  placeholder="E-post"> <?php echo $email_error;?>	<br> <br>
-			<input name="password" type= "password"  placeholder="parool"> <?php echo $email_error;?> <br> <br>
+			<input name="email" type= "email"  placeholder="E-post" >* <?php echo $email_error; ?> <br> <br>
+			<input name="password" type= "password"  placeholder="Parool" >* <?php echo $password_error; ?> <br> <br>
 		
 			<input type="submit" value= "Logi sisse"> 
 		</form>
-		<h2>Create user</h2>
+		
+		<h2>Loo kasutaja</h2>
+		<form action="user_form.php" method="post">
+			Sisesta E-posti aadres: <input name="email" type= "email"  placeholder="E-post" >* <?php echo $email_error; ?> <br> <br>
+			Sisesta parool: <input name="password" type= "password"  placeholder="Parool" >* <?php echo $password_error; ?> <br> <br>
+			Eesnimi: <input name="Eesnimi" type= "text"  placeholder="Eesnimi"" >* <?php echo $eesnimi_error; ?> <br> <br>
+		
+			<input type="submit" value= "Loo kasutaja"> 
+		</form>
+		
+		<h4>Mul üks väike mvp idee on, aga sellest ma praegu ei kirjuta. Pole veel selles idees kindel. Loodan, et järgmine kord on mingist ideest midagi rääkida ;) </h4>
 		
 	
 	</body>
