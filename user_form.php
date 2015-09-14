369449b0eac2dfcdf01b6d41488e53e7682ce13d
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-09-14 15:37:29 +0300

    kodune töö tunnis tehtud

--- user_form.php
+++ user_form.php
@@ -6,51 +6,110 @@
 	//jutumarkide vahele input elemendi NAME
 	//echo $_POST["email"];
 	//echo $_POST["password"];
-	$email_error = "";
-	$password_error = "";
+	//Errorid
+	
+	$loginemail_error = "";
+	$loginpassword_error = "";
+	$createemail_error = "";
+	$createpassword_error = "";
+	
+	//muutujad valuedega
+	$loginemail = "";
+	$loginpassword = "";
+	$createemail = "";
+	$createpassword = "";
+	
 	
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		
-		//kontrollime kasutaja eposti lahtrit, et see ei oleks tyhi
+		//kontrollin kas muutuja $_POST["login"], ehk login nupp 
+		if (isset($_POST["login"])){
+		
+			//kontrollime kasutaja eposti lahtrit, et see ei oleks tyhi
+		
+			if (empty($_POST["loginemail"])){
+				$loginemail_error = "Emaili ei tohi tyhjaks j2tta";
+			} else {
+				
+				//annan value
+				$loginemail = $_POST ["loginemail"];
+			}
+			
+			
+			// kontrollime parooli
+			if (empty($_POST["loginpassword"])){
+				$loginpassword_error = "Parooli ei tohi tyhjaks j2tta";
+			} else {
+			//kontrollime pikkust
+			
+			}
+			if ($loginpassword_error == "" && $loginemail_error == "")
+			{
+				
+				//erroreid polnud
+				echo "Kontrollin".$loginemail. " ".$loginpassword;
+			}	
+			
+		} elseif (isset($_POST["Create"])){
+		
+		//if (strlen($_POST["password"]) <8 ){
+		//	$password_error = "Peab olema vahemalt 8 symbolit";
+		//}
+		
+		if (empty($_POST["createemail"])){
+				$createemail_error = "Emaili ei tohi tyhjaks j2tta";
+			} else {
+				
+				//annan value
+				$createemail = $_POST ["createemail"];
+			}
+			
+			
+			// kontrollime parooli
+			if (empty($_POST["createpassword"])){
+				$createpassword_error = "Parooli ei tohi tyhjaks j2tta";
+			} else {
+			//kontrollime pikkust
+			
+			}
+			if ($createpassword_error == "" && $createemail_error == "")
+			{
+				
+				//erroreid polnud
+				echo "Kontrollin".$createemail. " ".$createpassword;
+			
+			}
+		}
+		
+	}
 	
-		if (empty($_POST["email"])){
-			$email_error = "Emaili ei tohi tyhjaks j2tta";
-		}
-		
-		if (empty($_POST["password"])){
-			$password_error = "Parooli ei tohi tyhjaks j2tta";
-		} else {
-		//kontrollime pikkust
-		
-		if (strlen($_POST["password"]) <8 ){
-			$password_error = "Peab olema vahemalt 8 symbolit";
-		}
-		
-	}
-	}
+	
 	
 	//kontrolli ainult siis kui kasutaja vajutab login nuppu
 	
 	
 	?>
+	<?php
+//lehe nimi
+//$page_title = "Login";
+//faili nimi
+//$page_file_name = "login.php";
+?>
+
 	
-	<html>
-		<head>
-			<Title>User login page</title>
-		</head>
-			<body>
+	
 			
 				<h2>Login</h2>
-					<form action="user_form.php" method="post">
-						<input name="email" type="email" placeholder="E-post" > * <?php echo $email_error; ?> <br> <br> 
-						<input name="password" type="password" placeholder="Parool" > * <?php echo $password_error ; ?><br><br>
+					<form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] )?>" method="post">
+						<input name="loginemail" type="email" placeholder="E-post" value ="<?php echo $loginemail; ?>" > * <?php echo $loginemail_error; ?> <br> <br> 
+						<input name="loginpassword" type="password" placeholder="Parool" > * <?php echo $loginpassword_error ; ?><br><br>
 						
-						<input type="submit" value="Logi sisse">
+						<input name="login" type="submit" value="Login">
 					</form>
 				<h2>Create user</h2>
-					<form action="user_form.php" method="post">
-					<input name="email" type="email" placeholder="E-post" > * <?php echo $email_error; ?> <br> <br> 
-					<input name="password" type="password" placeholder="Parool" > * <?php echo $password_error ; ?><br><br>
+					<form action="<?php echo htmlspecialchars ( $_SERVER["PHP_SELF"] )?>" method="post">
+						<input name="createemail" type="email" placeholder="E-post" value ="<?php echo $createemail; ?>" > * <?php echo $createemail_error; ?> <br> <br> 
+						<input name="createpassword" type="password" placeholder="Parool" > * <?php echo $createpassword_error ; ?><br><br>
 						
-			</body>
-	</html>
+						<input name="Create" type="submit" value="Create">
+			
