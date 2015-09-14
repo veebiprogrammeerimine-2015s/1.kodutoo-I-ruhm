83e993b4b6fb815e84383f410848978d9f5961ca
Kelly Leppik <kelllep@greeny.cs.tlu.ee>
2015-09-14 12:35:52 +0300

    kodutoo parandus

--- user_form.php
+++ user_form.php
@@ -8,7 +8,11 @@
 		
 	$email_error = "";
 	$password_error = "";
-	$eesnimi_error = "";
+	$first_name_error = "";
+	$new_email_error = "";
+	$new_password_error = "";
+	
+	
 	
 	
 	//Kontrolli ainult siis kui kasutaja vajutab logi sisse nuppu
@@ -31,9 +35,25 @@
 			}
 		}
 		
+		//Kontrollime kasutaja e-posti, et see ei ole tühi
+		if(empty($_POST["new_email"])) {
+			$new_email_error = "ei saa olla tühi";
+		}	
+		
+		//Kontrolli parooli
+		if(empty($_POST["new_password"])) {
+			$new_password_error = "sisesta parool";
+		} else {
+			//Parool ei ole tühi, kontrollime pikkust
+			if(strlen($_POST["new_password"]) < 8 ){
+				
+				$new_password_error = "Peab olema vähemalt 8 sümbolit pikk";
+			}
+		}
+		
 		//ega nimi ei ole tühi
-		if (empty($_POST["eesnimi"])) {
-			$eesnimi_error = "Palun sisesta eesnimi";
+		if (empty($_POST["fist_name"])) {
+			$first_name_error = "Palun sisesta eesnimi";
 		}
 	}
 ?>
@@ -54,9 +74,9 @@
 		
 		<h2>Loo kasutaja</h2>
 		<form action="user_form.php" method="post">
-			Sisesta E-posti aadres: <input name="email" type= "email"  placeholder="E-post" >* <?php echo $email_error; ?> <br> <br>
-			Sisesta parool: <input name="password" type= "password"  placeholder="Parool" >* <?php echo $password_error; ?> <br> <br>
-			Eesnimi: <input name="Eesnimi" type= "text"  placeholder="Eesnimi"" >* <?php echo $eesnimi_error; ?> <br> <br>
+			Sisesta E-posti aadres: <input name="new_email" type= "email"  placeholder="E-post" >* <?php echo $new_email_error; ?> <br> <br>
+			Sisesta parool: <input name="new_password" type= "password"  placeholder="Parool" >* <?php echo $new_password_error; ?> <br> <br>
+			Eesnimi: <input name="fist_name" type= "text"  placeholder="Eesnimi"" >* <?php echo $first_name_error; ?> <br> <br>
 		
 			<input type="submit" value= "Loo kasutaja"> 
 		</form>
