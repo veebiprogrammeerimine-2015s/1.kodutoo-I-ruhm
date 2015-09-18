55ad9cfbfda22f6b76fdc90e61c4cff2e3ce5107
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-09-18 13:41:34 +0300

    proovisin veel

--- user_form.php
+++ user_form.php
@@ -11,47 +11,50 @@
 	$password_error = "" ;
 	$email_2_error = "" ;
 	$password_2_error = "" ;
-	$comment_error = "" ;
 
-	$email="";
-	$password="";
+
+	//$email="";
+	//$password="";
 	
 	
 	// kontrolli ainult siis, kui kasutaja vajutab "logi sisse" nuppu
 	    if($_SERVER["REQUEST_METHOD"] == "POST"){
 					
-		//kontrollime, et e-post ei oleks tühi		
-		if(empty($_POST["email"])) { 
+		//kontrollime, mis nuppu vajutati		
+		if(isset($_POST["login"])){ 
+		//kontrollime, et e-post ei oleks tühi	
+			if( empty($_POST["email"])){
+				
+			
 			$email_error = "Ei saa olla tühi";
 		} else {
 			
 		$email = input($_POST["email"]);}
+		
 		//kontrollime parooli	
-		if(empty($_POST["password"])) { 
+		if(empty($_POST["password"])); { 
 			$password_error = "Ei saa olla tühi";
-		} else {$password = input($_POST["password"]);
-		}
+		} 
+		
 		//parooli pikkuse kontroll, kui see ei ole tühi			
 			if(strlen($_POST["password"])<8){
 				
-				$password_2_error = "Peab olema vähemalt kaheksa sümbolit pikk";
-		}  else {
-			
-		}
+				$password_error = "Peab olema vähemalt kaheksa sümbolit pikk";
+		}  
+		
+		} elseif(isset($_POST["create"])){
 		
 	 
-		if(empty($_POST["email_2"])) { 
+		    if(empty($_POST["email_2"])) { 
 			$email_2_error = "Ei saa olla täitmata";
-		} 	 else {
-			
+		} 	else {
 		}
-			if(empty($_POST["password"])) { 
+			if(empty($_POST["password_2"])) { 
 			$password_2_error = "Ei saa olla täitmata";
 		
-		}	else {
-		}
+		}	
 	}
-	
+	}
 		
 ?>
 
@@ -72,7 +75,7 @@
 		<h2>Create user</h2>
 			<form action="user_form.php" method="post">
 			<input name="email_2" type="email" placeholder="E-post">* <?php echo $email_2_error; ?><br> <br>
-			<input name="password" type="password" placeholder="parool">* <?php echo $password_2_error; ?> <br> <br>
+			<input name="password_2" type="password" placeholder="parool">* <?php echo $password_2_error; ?> <br> <br>
 			<input name="comment" type="text" placeholder="comment"> <br> <br> 
 			<textarea name="comment1" type="text" cols= "40" rows= "5" placeholder="Lisainfo"></textarea> <br> <br>
 			<textarea name="comment2" type="text" cols= "60" rows= "5"> Enda tööks planeerin trennipäeviku koostamise. Tegemist võiks olla sellise asjaga, kuhu inimene kirjutab, et mis päevadel ja mida ta täpselt tegi. Andmete põhjal saaks siis teha erinevaid arvutusi ja järeldusi. Kasutajaid võib olla ka mitu.</textarea> <br> <br>
