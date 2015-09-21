b669989d32870f429c3656472ca030962238e155
Kristian Talviste <kristal@greeny.cs.tlu.ee>
2015-09-21 14:30:32 +0300

    create user

--- login.php
+++ login.php
@@ -15,3 +15,24 @@
 			}else{
 				$password = cleanInput($_POST["password"]);
 			}
+			
+ // *********************
+    // ** LOO KASUTAJA *****
+    // *********************
+    if(isset($_POST["create"])){
+
+			if ( empty($_POST["create_email"]) ) {
+				$create_email_error = "See väli on kohustuslik";
+			}else{
+				$create_email = cleanInput($_POST["create_email"]);
+			}
+
+			if ( empty($_POST["create_password"]) ) {
+				$create_password_error = "See väli on kohustuslik";
+			} else {
+				if(strlen($_POST["create_password"]) < 8) {
+					$create_password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+				}else{
+					$create_password = cleanInput($_POST["create_password"]);
+				}
+			}
