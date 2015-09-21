abd65c6477dd44d6342bf0cfe7bb66634d819ed5
Kristian Talviste <kristal@greeny.cs.tlu.ee>
2015-09-21 14:19:50 +0300

    login

---
+++ login.php
@@ -0,0 +1,17 @@
+    // *********************
+    // **** LOGI SISSE *****
+    // *********************
+		if(isset($_POST["login"])){
+
+			if ( empty($_POST["email"]) ) {
+				$email_error = "See väli on kohustuslik";
+			}else{
+        // puhastame muutuja võimalikest üleliigsetest sümbolitest
+				$email = cleanInput($_POST["email"]);
+			}
+
+			if ( empty($_POST["password"]) ) {
+				$password_error = "See väli on kohustuslik";
+			}else{
+				$password = cleanInput($_POST["password"]);
+			}
