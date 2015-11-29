e5611efb235146bb5ba8f7d42a692a9d212d5a81
Stenver Kööp <stenkoo@greeny.cs.tlu.ee>
2015-11-29 14:35:27 +0200

    first_name and last_name fix

--- kodutoo.php
+++ kodutoo.php
@@ -1,14 +1,14 @@
 <?php
 
 	//kodutoo.php
-	
+
 	//jutumärkide vahele input elemndi NAME
 	//echo $_POST["email"];
 	//echo $_POST["password"];
-	
+
 	$email_error = "";
 	$password_error = "";
-	
+
 	// Kontrolli ainult siis kui kasutaja vajutab logi sisse nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -16,17 +16,17 @@
 		if(empty($_POST["email"])) {
 		$email_error = "* Ei tohi jääda tühjaks";
 		}
-		
+
 		//Kontrollime parooli
 		if(empty($_POST["password"])) {
 		$password_error = "* Ei tohi jääda tühjaks";
 		}else {
-			
+
 				//parool ei ole tühi, kontrollime pikkust
 				if(strlen($_POST["password"]) < 8 ){
-					
+
 					$password_error = "Peab olema vähemalt 8 sümbolit pikk";
-					
+
 				}
 		
 		}
@@ -37,18 +37,18 @@
 		<title>User login page</title>
 	</head>
 	<body>
-		
+
 		<h2>Login</h2>
 		<form action="kodutoo.php" method="post">
 			<input name="email" type="email"  placeholder="e-post@kool.ee"> <?php echo $email_error; ?> <br> <br>
 			<input name="password" type="password"  placeholder="parool"> <?php echo $password_error; ?> <br> <br>
 		<form>
 			<input type="submit" value="Logi sisse">
-		
+
 		<h2>Create user</h2>
 		<form action="kodutoo.php" method="post">
-			<input name="name" type="text"  placeholder="Eesnimi"> <br> <br>
-			<input name="name" type="text"  placeholder="Perenimi"> <br> <br>
+			<input name="first_name" type="text"  placeholder="Eesnimi"> <br> <br>
+			<input name="last_name" type="text"  placeholder="Perenimi"> <br> <br>
 			<input name="email" type="email"  placeholder="e-post@kool.ee"> <?php echo $email_error; ?> <br> <br>
 			<input name="password" type="password"  placeholder="parool"> <?php echo $password_error; ?> <br> <br>
 		<form>
