de08a9902901e946a985fe9cb91102a73bf71439
Rasmus Reichardt <rasmrei@greeny.cs.tlu.ee>
2016-01-16 12:35:11 +0200

    1.kodutoo

--- user_form.php
+++ user_form.php
@@ -10,23 +10,26 @@
 
 	// Kontrollib ainult siis kui kasutaja vajutab logi sisse nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+	if(isset($_POST["login"])){ 
 
 		//Kontrollib kasutaja e-posti et see ei ole tühi
 		if(empty($_POST["email"])) {
-		$email_error = "* Ei tohi jääda tühjaks";
-		}
+		$email_error = "Ei tohi jääda tühjaks";
+		} else {
+			
+		$email = input($_POST["email"]);}
 
 		//Kontrollib parooli
 		if(empty($_POST["password"])) {
 		$password_error = "* Ei tohi jääda tühjaks";
-		}else {
+		}
 
-				//parool ei ole tühi, siis kontrollime pikkust
-				if(strlen($_POST["password"]) < 8 ){
+			//parool ei ole tühi, siis kontrollime pikkust
+			if(strlen($_POST["password"]) < 8 ){
 
-					$password_error = "Peab olema vähemalt 8 sümbolit pikk";
-
-				}
+				$password_error = "Peab olema vähemalt 8 sümbolit pikk";
+			}
 		
 		}
 	}
@@ -40,21 +43,20 @@
 	<body>
 
 		<h2>Login</h2>
-		<form action="kodutoo.php" method="post">
+		<form action="user_form.php" method="post">
 			<input name="email" type="email"  placeholder="e-post"> <?php echo $email_error; ?> <br> <br>
 			<input name="password" type="password"  placeholder="parool"> <?php echo $password_error; ?> <br> <br>
+			<input name="login" type="submit" value="Logi sisse">
 		<form>
-			<input type="submit" value="Logi sisse">
-
+			
 		<h2>Create user</h2>
-		<form action="kodutoo.php" method="post">
+		<form action="user_form" method="post">
 			<input name="first_name" type="text"  placeholder="Eesnimi"> <br> <br>
 			<input name="last_name" type="text"  placeholder="Perenimi"> <br> <br>
 			<input name="email" type="email"  placeholder="e-post"> <?php echo $email_error; ?> <br> <br>
 			<input name="password" type="password"  placeholder="parool"> <?php echo $password_error; ?> <br> <br>
+			<input name="create" type="submit" value="Loo kasutaja">
 		<form>
-			<input type="submit" value="Loo kasutaja">
-
+			
 	</body>
-
 </html>
